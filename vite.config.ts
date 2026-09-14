88a19160b6cce3c8f71cd45886c6dfca94f79aa2
spherestrikegames <rishi.p1.goyal@gmail.com>
2026-09-14 16:21:04 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
+   base: '/Calculator/',
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
